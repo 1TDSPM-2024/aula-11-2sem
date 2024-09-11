620841194ccda0db3f72efd9a12279774bed5724
GuiRomanholi <guilhermeroma343@gmail.com>
2024-09-11 09:44:33 -0300

    Tentando Fazer o DarkMode

--- aula11/src/components/Cabecalho/Cabecalho.tsx
+++ aula11/src/components/Cabecalho/Cabecalho.tsx
@@ -1,12 +1,24 @@
-import "../../style/style.scss"
 import Menu from "../Menu/Menu";
+import { useState } from "react";
+import { CgDarkMode as BtDark } from "react-icons/cg";
+import "../../style/style.scss"
 
 export default function Cabecalho(){
+
+    const [tema, setTema] = useState<"light" | "dark">("light");
+
+    const lancadorTema = () => {
+      setTema((prevTema) => (prevTema === "light" ? "dark" : "light"))
+    }
+
     return(
-        <header>
+        <div className={`app ${tema}`}>
+        <header className="app-header">
+            <BtDark onClick={lancadorTema} className="btn-cg"/>
             <h1>Cabeçalho</h1>
             <Menu/>
         </header>
+        </div>
     )
 
 }
