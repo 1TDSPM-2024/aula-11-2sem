038d20da400d7dc04107a193ec84f3a070115797
nyxtais <nyxtais@gmail.com>
2024-09-11 09:47:43 -0300

    11/09

--- aula11/src/routes/Usuarios/index.tsx
+++ aula11/src/routes/Usuarios/index.tsx
@@ -1,6 +1,39 @@
+import { useEffect, useState } from "react";
+import { TipoUsuario } from "../../types";
 
 export default function Usuarios(){
 
+  const [usuarios, setUsuarios] = useState<TipoUsuario[]>([{
+    id: 0,
+    login: "",
+    avatar_url: "",
+    url: ""
+  }])
+
+  //Async e Await
+
+  useEffect(() => {
+
+    async function usersGit() {
+
+      try{
+        const response = await fetch("https://api.github.com/users");
+
+        if (!response.ok) {
+          throw new Error("Dados solicitados incorretos!");
+        }
+        const data = await response.json();
+        setUsuarios(data);
+
+      }catch (err){
+        console.log(err);
+      }
+    }
+    usersGit();
+  }, [])
+
+/*
+  useEffect(() => {
     fetch("https://api.github.com/users")
     .then(response=>{
       if(!response.ok){
@@ -10,14 +43,23 @@ export default function Usuarios(){
     })
     .then(data=>{
       console.log(data)
+      setUsuarios(data);
     })
     .catch(err=>{
       console.log(err.message)
     })
+  }, [])
+  */
 
     return(
       <div>
         <h1>Bem Vindos Usuários</h1>
+
+        <ul>
+          {usuarios.map((u) => (
+            <li key={u.id}>{u.login} - <img style={{ width: "2%", cursor: "pointer" }} src={u.avatar_url} alt={u.login} /></li>
+          ))}
+        </ul>
       </div>
     );
   }
