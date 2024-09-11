06016ab097e23dc32d33a8ee9fbb81fb807da6f2
LuizHNR <luizhneri12@gmail.com>
2024-09-11 09:16:02 -0300

    começo dark mode :)

--- aula11/src/components/Cabecalho/Cabecalho.tsx
+++ aula11/src/components/Cabecalho/Cabecalho.tsx
@@ -1,12 +1,24 @@
-import "../../style/style.scss"
 import Menu from "../Menu/Menu";
+import { useState } from "react";
+import { MdOutlineWbSunny as btDark } from "react-icons/md";
+import "../../style/style.scss"
 
 export default function Cabecalho(){
+
+    const [tema, setTema] = useState<"light" | "dark">("light");
+
+    const lancadorTema = ()=>{
+        setTema((prevTema))
+    }
+
     return(
-        <header>
+        <div className={'app ${tema}'}>
+        <header className="app-header">
             <h1>Cabeçalho</h1>
             <Menu/>
         </header>
+        </div>
+        
     )
 
 }
