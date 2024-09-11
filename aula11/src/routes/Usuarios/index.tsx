6d7ee45058096050393cad49c563fb165eafa482
LuizHNR <luizhneri12@gmail.com>
2024-09-11 08:48:10 -0300

    :) Chamada de API com promise e apresentação

--- aula11/src/routes/Usuarios/index.tsx
+++ aula11/src/routes/Usuarios/index.tsx
@@ -1,6 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { TipoUsuario } from "../../types";
 
 export default function Usuarios(){
+
+  const [usuarios, setUsuarios] = useState<TipoUsuario[]>([{
+    id: 0,
+    login: "",
+    avatar_url: "",
+    url: ""
+  }]);
+
   useEffect(()=>{
     fetch("https://api.github.com/users")
     .then(response=>{
@@ -10,7 +19,8 @@ export default function Usuarios(){
       return response.json()
     })
     .then(data=>{
-      console.log(data)
+      console.log(data);
+      setUsuarios(data);
     })
     .catch(err=>{
       console.log(err.message)
@@ -20,6 +30,12 @@ export default function Usuarios(){
     return(
       <div>
         <h1>Bem Vindos Usuários</h1>
+
+            {usuarios.map((u)=>(
+              <li key={u.id}>{u.login} - <img style={{width: "2%", cursor: "pointer"}} src={u.avatar_url} alt={u.login} /></li>
+            ))}
+
+    
       </div>
     );
   }
