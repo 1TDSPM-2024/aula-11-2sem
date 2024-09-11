09c8241c9283db9fc0dd183d1a590c71fd2bf23f
irissuu <iriswinx2@gmail.com>
2024-09-11 09:53:04 -0300

    aula11

--- aula11/src/routes/Usuarios/index.tsx
+++ aula11/src/routes/Usuarios/index.tsx
@@ -1,23 +1,60 @@
+import { useEffect, useState } from "react";
+import { TipoUsuario } from "../../types";
 
-export default function Usuarios(){
+export default function Usuarios() {
 
-    fetch("https://api.github.com/users")
-    .then(response=>{
-      if(!response.ok){
-        throw new Error("Dados solicitados incorretos!");
+  const [usuarios, setUsuarios] = useState<TipoUsuario[]>([{
+    id: 0,
+    login: "",
+    avatar_url: "",
+    url: ""
+  }])
+
+  //ASYNC e AWAIT
+
+  useEffect(() => {
+    async function usersGit() {
+      try{
+        const response = await fetch("https://api.github.com/users");
+        if (!response.ok) {
+          throw new Error("Dados solicitados incorretos!");
+        }
+        const data =  await response.json();
+        setUsuarios(data);
+      }catch (err){
+        console.log(err);
       }
-      return response.json()
-    })
-    .then(data=>{
-      console.log(data)
-    })
-    .catch(err=>{
-      console.log(err.message)
-    })
+}
+usersGit();
+    }, [])
 
-    return(
-      <div>
-        <h1>Bem Vindos Usuários</h1>
-      </div>
-    );
+// useEffect(() => {
+//   fetch("https://api.github.com/users")
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error("Dados solicitados incorretos!");
+//       }
+//       return response.json()
+//     })
+//     .then(data => {
+//       console.log(data);
+//       setUsuarios(data);
+//     })
+//     .catch(err => {
+//       console.log(err.message)
+//     })
+// }, [])
+
+return (
+  <div>
+    <h1>Bem Vindos Usuários</h1>
+
+    <ul>
+      {usuarios.map((u) => (
+        <li key={u.id}>{u.login} - <img style={{ width: "2%", cursor: "pointer" }} src={u.avatar_url} alt={u.login} /></li>
+      ))}
+    </ul>
+  </div>
+);
+
   }
