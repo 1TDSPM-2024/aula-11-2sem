9c61df8bdcab3681a82d0b6f0c0e8efe4beba44e
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-09-11 09:43:37 -0300

    Semi Darkmode no Cabeçalho

--- aula11/src/components/Cabecalho/Cabecalho.tsx
+++ aula11/src/components/Cabecalho/Cabecalho.tsx
@@ -1,12 +1,23 @@
-import "../../style/style.scss"
 import Menu from "../Menu/Menu";
+import { useState } from "react";
+import { CgDarkMode as BtDark } from "react-icons/cg";
+import "../../style/style.scss"
 
 export default function Cabecalho(){
+
+    const [tema, setTema] = useState<"light" | "dark">("light");
+
+    const lancadorTema = () => {
+      setTema((prevTema) => (prevTema === "light" ? "dark" : "light"))
+    }
+    
     return(
-        <header>
+        <header className={`app ${tema}`}>
+            <BtDark onClick={lancadorTema}/>
             <h1>Cabeçalho</h1>
             <Menu/>
         </header>
+        
     )
 
 }
